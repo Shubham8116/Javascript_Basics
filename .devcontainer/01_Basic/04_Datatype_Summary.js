98c72a1b5d66df245faabf943d783fb552f60364
Shubham Uprikar <167060098+Shubham8116@users.noreply.github.com>
2025-04-21 18:23:52 +0000

    datatype summary

--- .devcontainer/01_Basic/04_Datatype_Summary.js
+++ .devcontainer/01_Basic/04_Datatype_Summary.js
@@ -30,3 +30,27 @@ let myFunction= function()
 }
 
 console.log(typeof myFunction)//prints function
+
+console.log(names[0])
+console.log(values.id)
+
+/* *************** Stack and Heap Memory ******************* */
+
+
+//primitive datatypes store in stack while non primitive datatype store in heap
+
+let inputOne={          //inputOne will store in stack and get reference to heap memory for following data
+    email:"shubham.com", // email will store in heap
+    city:"nagpur" // city will store in heap
+}
+let inputTwo=inputOne // inputTwo will get reference of inputOne
+inputTwo.email="hello.com"
+console.log(inputOne.email)//"hello.com"
+console.log(inputTwo.email)//"hello.com"
+
+// but in case of stack memory values not changes
+let num1=10
+let num2=num1
+num2=20
+console.log(num1)//10
+console.log(num2)//20
