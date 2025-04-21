09fe4f6fdfbfcfac4663183b1792496b2379f275
Shubham Uprikar <167060098+Shubham8116@users.noreply.github.com>
2025-04-21 20:26:57 +0000

    date and time

---
+++ .devcontainer/01_Basic/07_Date_time.js
@@ -0,0 +1,20 @@
+let myDate=new Date()
+console.log(typeof myDate)//object
+console.log(myDate.toString())//Mon Apr 21 2025 20:09:16 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleString())//4/21/2025, 8:09:16 PM
+console.log(myDate.toDateString())// Mon Apr 21 2025
+
+let mycreatedDate=new Date(2025,3,10)
+console.log(mycreatedDate.toDateString()) //month start from 0 - Thu Apr 10 2025
+
+let myCreatedNewDate=new Date("2025-01-14")// here month start from 1 Tue Jan 14 2025
+console.log(myCreatedNewDate.toDateString())
+
+let newDate =Date.now()
+console.log(newDate)//1745266758075 in miliseconds
+console.log(mycreatedDate.getDate())
+console.log(mycreatedDate.getDay())
+console.log(mycreatedDate.getMonth()+1) // +1 this is for month starts from 0 so, add one to it
+console.log(mycreatedDate.getTime())
+console.log(mycreatedDate.getHours())
+
