cf933fc4c45b1f42dd9b30b9f11a3a276f3ffa4c
Shubham Uprikar <167060098+Shubham8116@users.noreply.github.com>
2025-04-21 15:12:06 +0000

    datatypes

---
+++ .devcontainer/01_Basic/02_Datatypes.js
@@ -0,0 +1,13 @@
+"use strict"// treat all js code as newer version
+let name="shubham";
+let number=90;
+let city;//undefined
+let address=null;// standalone value
+let isLoggedIn=true//boolean
+//bigint for large number
+//symbol for unique
+//object
+console.log(typeof "shubham")//prints string
+console.log(typeof 30)//prints number
+console.log(typeof undefined) //prints undefined
+console.log(typeof null)// prints object
